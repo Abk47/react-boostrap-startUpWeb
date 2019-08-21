b98084204ca35fb0cb63087ca2dc761fc90ddffb
Abuka <maacsgaming@gmail.com>
2019-08-22 01:29:06 +0300

    add header component and props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Header from './Header'
 
-const Home = () =>{
-  return (
-    <div>
-      <header className='masthead'>
-        <div className='container'>
-          <div className='intro-text'>
-            <div className='intro-lead-in'>Welcome To Our Studio!</div>
-            <div className='intro-heading text-uppercase'>It's Nice To Meet You</div>
-            <a className='btn btn-primary btn-xl text-uppercase js-scroll-trigger' href='#services'>Tell Me More</a>
-          </div>
-        </div>
-      </header>
-    </div>
-  )
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <Header title='Welcome To Our Studio!'
+          subtitle="IT'S NICE TO MEET YOU"
+          textButton='LEARN MORE'
+          showButton
+          link='/services' />
+      </div>
+    )
+  }
 }
-
 export default Home
