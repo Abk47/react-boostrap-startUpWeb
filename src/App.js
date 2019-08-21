f5e34c061a61f1171c0e2b37516161eb5a32f65e
Abuka <maacsgaming@gmail.com>
2019-08-21 16:29:00 +0300

    add react-router-dom dependancy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
+import {BrowserRouter as Router,Route, Link} as 'react-router-dom'
 import './App.css'
 import Navigation from './components/Navigation'
-import Header from './components/Header';
+import Header from './components/Header'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
 
 const App = () => {
   return (
     <Fragment>
-      <Navigation />
-      <Header />
+      <Navigation>
+        <Header />
+      <Navigation/>
+      <Services />
+      <Portfolio />
     </Fragment>
   )
 }
